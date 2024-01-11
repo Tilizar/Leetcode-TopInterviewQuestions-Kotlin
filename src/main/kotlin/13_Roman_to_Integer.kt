83e8cf3bd96c7f74cfefd4041ac15204f55e8078
Tilizar <a.matiushin.s@gmail.com>
2024-01-11 10:45:39 +0300

    Solution for 13. Roman to Integer

---
+++ src/main/kotlin/13_Roman_to_Integer.kt
@@ -0,0 +1,32 @@
+fun romanToInt(s: String): Int {
+    val romanToInt = mapOf(
+        'I' to 1,
+        'V' to 5,
+        'X' to 10,
+        'L' to 50,
+        'C' to 100,
+        'D' to 500,
+        'M' to 1000
+    )
+
+    var sum = 0
+
+    s.forEachIndexed { index, c ->
+        val current = romanToInt[c]!!
+        val next = if (index + 1 < s.length) romanToInt[s[index + 1]] else null
+
+        if (next != null && current < next) {
+            sum -= current
+        } else {
+            sum += current
+        }
+    }
+
+    return sum
+}
+
+fun main() {
+    assert(3 == romanToInt("III"))
+    assert(58 == romanToInt("LVIII"))
+    assert(1994 == romanToInt("MCMXCIV"))
+}
